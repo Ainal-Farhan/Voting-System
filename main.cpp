66ed9c4ccfcbeb2b019c5dd271000028ff353720
Ainal-Farhan <56626447+Ainal-Farhan@users.noreply.github.com>
2019-12-11 10:19:31 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+class Voter{
+  public:
+    Voter* first;
+    Voter* next;
+    string name;
+    string ic;
+    int age;
+    bool voteStatus;
+};
+
 int main(){
   return 0;
 }
